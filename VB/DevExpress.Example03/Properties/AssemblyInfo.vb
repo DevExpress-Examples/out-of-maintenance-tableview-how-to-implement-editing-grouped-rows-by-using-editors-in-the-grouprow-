5bbc361937fb36c6d998e81a5ff74d34884d6a2d
DevExpressExampleBot <fakeEmail>
2023-01-03 20:25:31 +0400

    Source auto update [skip ci]

--- VB/DevExpress.Example03/Properties/AssemblyInfo.vb
+++ VB/DevExpress.Example03/Properties/AssemblyInfo.vb
@@ -1,24 +1,3 @@
-' Developer Express Code Central Example:
-' How to create Checkable Grouping in GridControl
-' 
-' The example demonstrates how to add the CheckBox functionality to each GroupRow
-' in TableView. The basic idea is to allow a user to easily check or uncheck the
-' necessary group of items in GridControl.
-' 
-' The functionality is realized as
-' behavior in the CheckableGroupBehavior class, which can be attached to
-' GridControl. It automatically sets GroupValueTemplate using the GroupCheckBox
-' class, which is inherited from the CheckBox class. The CheckableGroupBehavior's
-' CheckableProperty must be set and has to contain the name of the property in a
-' row data object, which will be used to check items. The property has to be of
-' the Boolean type.
-' 
-' You can easily add the same functionality to your project by
-' using the CheckableGroupBehavior class and attaching it as behavior to your
-' GridControl.
-' 
-' You can find sample updates and versions for different programming languages here:
-' http://www.devexpress.com/example=T127563
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 Imports System.Windows
